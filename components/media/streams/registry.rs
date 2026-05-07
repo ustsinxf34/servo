ustsinxf34/servo

commit 99c72bfb16cd721b427e9a9b7a7a200ed8c57854

deps: Merge the servo/media repository (#42369)
This change merges http://github.com/servo/media into this repository.
It is only used by Servo and version upgrades are complicated by having
two repositories. In addition, this avoids the need to refer to
individual commit hashes in the Servo `Cargo.toml`. The hope is that
merging these two repositories will lead to better code organization /
simplification like we have seen with the WebXR support. Initiailly, the
idea was that this media support could be shared with the wider Rust
ecosystem, but I think that hasn't worked out as planned due to the fact
that it is difficult to use the various media packaes outside of the
Servo project and the fact that no one seems to be doing this.

Some changes were made when importing the code:
- The second commit in this PR addresses new clippy warnings from the
imported code.
 - GStreamer Packages are no longer renamed in the media code, so that
   they are named the same as they are currently in Servo.
 - Some examples are not ported as they require being run interactively
   and depend on older version of important libraries like winit.
   Having these dependencies in the core part of Servo isn't very
   convenient. Removed examples:
   - `audio_decoder.rs`: This is meant to be run interactively with a
     file so isn't very useful for testing.
   - Depended on winit GUI (`player` subdirectory):
     - `media_element_source_node.rs`
     - `play_media_stream.rs`
     - `simple_player.rs`
     - `muted_player.rs`
   - `siple_webrtc.rs`: Depended on `webrtc` library:

Testing: This is covered by existing tests. In addition, the job which
runs
the media examples is added to the unit test workflow.

---------

Signed-off-by: Martin Robinson <mrobinson@igalia.com>

components/media/streams/registry.rs
@@ -1,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+use std::collections::HashMap;
+use std::sync::{Arc, LazyLock, Mutex};
+
+use uuid::Uuid;
+
+use super::MediaStream;
+
+type RegisteredMediaStream = Arc<Mutex<dyn MediaStream>>;
+
+static MEDIA_STREAMS_REGISTRY: LazyLock<Mutex<HashMap<MediaStreamId, RegisteredMediaStream>>> =
+    LazyLock::new(|| Mutex::new(HashMap::new()));
+
+#[derive(Clone, Copy, Default, Hash, Eq, PartialEq)]
+pub struct MediaStreamId(Uuid);
+impl MediaStreamId {
+    pub fn new() -> MediaStreamId {
+        Default::default()
+    }
+
+    pub fn id(self) -> Uuid {
+        self.0
+    }
+}
+
+pub fn register_stream(stream: Arc<Mutex<dyn MediaStream>>) -> MediaStreamId {
+    let id = MediaStreamId::new();
+    stream.lock().unwrap().set_id(id);
+    MEDIA_STREAMS_REGISTRY.lock().unwrap().insert(id, stream);
+    id
+}
+
+pub fn unregister_stream(stream: &MediaStreamId) {
+    MEDIA_STREAMS_REGISTRY.lock().unwrap().remove(stream);
+}
+
+pub fn get_stream(stream: &MediaStreamId) -> Option<Arc<Mutex<dyn MediaStream>>> {
+    MEDIA_STREAMS_REGISTRY.lock().unwrap().get(stream).cloned()
+}

tests/wpt/meta/webaudio/the-audio-api/the-mediastreamaudiodestinationnode-interface/ctor-mediastreamaudiodestination.html.ini
@@ -1,0 +1,2 @@
+[ctor-mediastreamaudiodestination.html]
+  expected: CRASH

tests/wpt/meta/webaudio/the-audio-api/the-mediastreamaudiosourcenode-interface/mediastreamaudiosourcenode-from-context-with-different-rate.https.html.ini
@@ -1,4 +1,5 @@
 [mediastreamaudiosourcenode-from-context-with-different-rate.https.html]
+  expected: CRASH
   [Test closing one AudioContext stops only it (cloned tracks)]
     expected: FAIL
 

tests/wpt/meta/webaudio/the-audio-api/the-mediastreamaudiosourcenode-interface/mediastreamaudiosourcenode-routing.html.ini
@@ -1,5 +1,4 @@
 [mediastreamaudiosourcenode-routing.html]
-  expected: TIMEOUT
+  expected: CRASH
   [MediaStreamAudioSourceNode captures the right track.]
     expected: TIMEOUT
-

commit 982e3a764ac2844bd9d0328ef132e00f18961f13

media: Make all media stream IDs contain a unique ID. (#44781)
Fixes a regression from #42369 that caused deadlocks whenever more than
one media stream was created.

Testing: Multiple WPT tests no longer timing out.
Fixes: #44778

Signed-off-by: Josh Matthews <josh@joshmatthews.net>

components/media/streams/registry.rs
@@ -15,15 +15,21 @@ type RegisteredMediaStream = Arc<Mutex<dyn MediaStream>>;
 static MEDIA_STREAMS_REGISTRY: LazyLock<Mutex<HashMap<MediaStreamId, RegisteredMediaStream>>> =
     LazyLock::new(|| Mutex::new(HashMap::new()));
 
-#[derive(Clone, Copy, Default, Hash, Eq, PartialEq, MallocSizeOf)]
+#[derive(Clone, Copy, Hash, Eq, PartialEq, MallocSizeOf)]
 pub struct MediaStreamId(Uuid);
 impl MediaStreamId {
     pub fn new() -> MediaStreamId {
-        Default::default()
+        Self(Uuid::new_v4())
     }
 
     pub fn id(self) -> Uuid {
         self.0
+    }
+}
+
+impl Default for MediaStreamId {
+    fn default() -> Self {
+        Self::new()
     }
 }
 

tests/wpt/meta/webaudio/the-audio-api/the-mediastreamaudiodestinationnode-interface/ctor-mediastreamaudiodestination.html.ini
@@ -1,2 +1,0 @@
-[ctor-mediastreamaudiodestination.html]
-  expected: CRASH

tests/wpt/meta/webaudio/the-audio-api/the-mediastreamaudiosourcenode-interface/mediastreamaudiosourcenode-from-context-with-different-rate.https.html.ini
@@ -1,5 +1,4 @@
 [mediastreamaudiosourcenode-from-context-with-different-rate.https.html]
-  expected: CRASH
   [Test closing one AudioContext stops only it (cloned tracks)]
     expected: FAIL
 
@@ -58,4 +57,226 @@
     expected: FAIL
 
   [Test removing tracks from one MediaStream silences only its detector when destination rates are the same (shared tracks, 48000->32000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences nothing (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences nothing (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences nothing (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences only its detector (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences nothing (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (cloned tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector when destination rates are the same (cloned tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@32000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamTrackAudioSourceNode@44100, MediaStreamTrackAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing one AudioContext stops only it (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending one AudioContext silences only it (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disconnecting one source node silences only it (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping one MediaStream's tracks silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test disabling one MediaStream's tracks silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test stopping the input MediaStream's tracks silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test suspending the input AudioContext silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test closing the input AudioContext silences all detectors (shared tracks, MediaStreamTrackAudioSourceNode@32000, MediaStreamAudioSourceNode@44100, MediaStreamAudioSourceNode@96000)]
+    expected: FAIL
+
+  [Test removing tracks from one MediaStream does not silence any detector when destination rates are the same (shared tracks, MediaStreamAudioSourceNode@32000, MediaStreamAudioSourceNode@32000)]
     expected: FAIL

tests/wpt/meta/webaudio/the-audio-api/the-mediastreamaudiosourcenode-interface/mediastreamaudiosourcenode-routing.html.ini
@@ -1,4 +1,4 @@
 [mediastreamaudiosourcenode-routing.html]
-  expected: CRASH
+  expected: TIMEOUT
   [MediaStreamAudioSourceNode captures the right track.]
     expected: TIMEOUT