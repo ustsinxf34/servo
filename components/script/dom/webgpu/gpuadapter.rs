ustsinxf34/servo

commit 474ee1726ce7df3c2314051264ab3c8612c076ad

script: Move DomRefCell, assert_in_* into script_bindings (#44598)
As part of splitting up the Script crate this moves DomRefCell (and
related) into script_bindings. Also it moves assert_in_script and
assert_in_layout, similarly.
As the diff is quite large, the major files are
- script_bindings/assert
- script_binding/cell
- script_binding/like 

Testing: As this is moving things around and changing visibility
compilation is the test.

Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

components/script/dom/webgpu/gpuadapter.rs
@@ -7,6 +7,7 @@ use std::rc::Rc;
 use dom_struct::dom_struct;
 use js::jsapi::{HandleObject, Heap, JSObject};
 use script_bindings::cformat;
+use script_bindings::like::Maplike;
 use webgpu_traits::{
     RequestDeviceError, WebGPU, WebGPUAdapter, WebGPUDeviceResponse, WebGPURequest,
 };
@@ -18,7 +19,6 @@ use crate::dom::bindings::codegen::Bindings::WebGPUBinding::{
     GPUAdapterMethods, GPUDeviceDescriptor, GPUDeviceLostReason,
 };
 use crate::dom::bindings::error::Error;
-use crate::dom::bindings::like::Setlike;
 use crate::dom::bindings::reflector::{DomGlobal, Reflector, reflect_dom_object};
 use crate::dom::bindings::root::{Dom, DomRoot};
 use crate::dom::bindings::str::DOMString;

commit 7ce89765e0a318e708831ef9c353a5512f7d52d0

script: Treat GPUAdapter as a set instead of a map. (#44823)
This fixes a regression from 474ee1726ce7df3c2314051264ab3c8612c076ad,
which replaced the Setlike import with Maplike. The Maplike
implementation for types that implement Setlike is mostly stubbed out.

Testing: Fixes panics in webgpu tests, but those results weren't updated
after the regressing change so there's no visible change in results.
Fixes: #44819

Signed-off-by: Josh Matthews <josh@joshmatthews.net>

components/script/dom/webgpu/gpuadapter.rs
@@ -7,7 +7,7 @@ use std::rc::Rc;
 use dom_struct::dom_struct;
 use js::jsapi::{HandleObject, Heap, JSObject};
 use script_bindings::cformat;
-use script_bindings::like::Maplike;
+use script_bindings::like::Setlike;
 use script_bindings::reflector::{Reflector, reflect_dom_object};
 use webgpu_traits::{
     RequestDeviceError, WebGPU, WebGPUAdapter, WebGPUDeviceResponse, WebGPURequest,