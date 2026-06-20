ustsinxf34/servo

commit db3456bf5b31ac85de098372c2945c90f90e4353

servoshell: Defaulting to a scheme with urls which specify a port (#45729)
When providing a url which has a port specified, the domain is
considered as the scheme. The url is updated to have a `http` scheme.

Testing: New tests with port have been added.
Fixes: #45572

---------

Signed-off-by: SteveSharonSam <157230657+SteveSharonSam@users.noreply.github.com>

ports/servoshell/parser.rs
@@ -42,6 +42,9 @@ pub fn get_default_url(
             ("file", None, Ok(ref path)) if exists(path) => {
                 new_url = cmdline_url;
             },
+            (scheme, None, Err(_)) if !is_normal_scheme(scheme) => {
+                new_url = ServoUrl::parse(&format!("http://{}:{}", scheme, &url.path())).ok();
+            },
             _ => {},
         }
     }
@@ -68,11 +71,19 @@ pub fn get_default_url(
 /// interpret the string as a search term.
 pub(crate) fn location_bar_input_to_url(request: &str, searchpage: &str) -> Option<ServoUrl> {
     let request = request.trim();
-    ServoUrl::parse(request)
-        .ok()
-        .or_else(|| try_as_file(request))
-        .or_else(|| try_as_domain(request))
-        .or_else(|| try_as_search_page(request, searchpage))
+    let input_url = ServoUrl::parse(request).ok();
+    if let Some(url) = input_url {
+        match (url.scheme(), url.host(), url.to_file_path()) {
+            (scheme, None, Err(_)) if !is_normal_scheme(scheme) => {
+                ServoUrl::parse(&format!("http://{}:{}", scheme, &url.path())).ok()
+            },
+            _ => Some(url),
+        }
+    } else {
+        try_as_file(request)
+            .or_else(|| try_as_domain(request))
+            .or_else(|| try_as_search_page(request, searchpage))
+    }
 }
 
 fn try_as_file(request: &str) -> Option<ServoUrl> {
@@ -99,4 +110,14 @@ fn try_as_search_page(request: &str, searchpage: &str) -> Option<ServoUrl> {
         return None;
     }
     ServoUrl::parse(&searchpage.replace("%s", request)).ok()
+}
+
+fn is_normal_scheme(s: &str) -> bool {
+    s == "file" ||
+        s == "http" ||
+        s == "https" ||
+        s == "ws" ||
+        s == "wss" ||
+        s == "ftp" ||
+        s == "data"
 }

ports/servoshell/test.rs
@@ -155,6 +155,12 @@ fn test_cmdline_and_location_bar_url() {
         "file:///fake/cwd/dragonfruit",
         "https://duckduckgo.com/html/?q=dragonfruit",
     );
+    test_url(
+        "localhost:8000",
+        "http://localhost:8000/",
+        "http://localhost:8000/",
+        "http://localhost:8000/",
+    )
 }
 
 #[test]
@@ -208,6 +214,12 @@ fn test_cmdline_and_location_bar_url() {
         "file:///C:/fake/cwd/dragonfruit",
         "https://duckduckgo.com/html/?q=dragonfruit",
     );
+    test_url(
+        "localhost:8000",
+        "http://localhost:8000/",
+        "http://localhost:8000/",
+        "http://localhost:8000/",
+    )
 }
 
 #[cfg(target_os = "linux")]

commit bee820085f13de7ffed9a905fe049a58175c7b92

servoshell: enabling loading of servo: URLs from command line and location bar (#45832)
Resolving a regression from #45729. The PR did not support custom servo:
URLs.

Testing: Added new tests.
Fixes: #45827

---------

Signed-off-by: SteveSharonSam <157230657+SteveSharonSam@users.noreply.github.com>

ports/servoshell/parser.rs
@@ -42,7 +42,7 @@ pub fn get_default_url(
             ("file", None, Ok(ref path)) if exists(path) => {
                 new_url = cmdline_url;
             },
-            (scheme, None, Err(_)) if !is_normal_scheme(scheme) => {
+            (scheme, None, Err(_)) if is_localhost(scheme) || is_domain_like(scheme) => {
                 new_url = ServoUrl::parse(&format!("http://{}:{}", scheme, &url.path())).ok();
             },
             _ => {},
@@ -74,7 +74,7 @@ pub(crate) fn location_bar_input_to_url(request: &str, searchpage: &str) -> Opti
     let input_url = ServoUrl::parse(request).ok();
     if let Some(url) = input_url {
         match (url.scheme(), url.host(), url.to_file_path()) {
-            (scheme, None, Err(_)) if !is_normal_scheme(scheme) => {
+            (scheme, None, Err(_)) if is_localhost(scheme) || is_domain_like(scheme) => {
                 ServoUrl::parse(&format!("http://{}:{}", scheme, &url.path())).ok()
             },
             _ => Some(url),
@@ -94,11 +94,6 @@ fn try_as_file(request: &str) -> Option<ServoUrl> {
 }
 
 fn try_as_domain(request: &str) -> Option<ServoUrl> {
-    fn is_domain_like(s: &str) -> bool {
-        !s.starts_with('/') && s.contains('/') ||
-            (!s.contains(' ') && !s.starts_with('.') && s.split('.').count() > 1)
-    }
-
     if !request.contains(' ') && is_reg_domain(request) || is_domain_like(request) {
         return ServoUrl::parse(&format!("https://{}", request)).ok();
     }
@@ -112,12 +107,11 @@ fn try_as_search_page(request: &str, searchpage: &str) -> Option<ServoUrl> {
     ServoUrl::parse(&searchpage.replace("%s", request)).ok()
 }
 
-fn is_normal_scheme(s: &str) -> bool {
-    s == "file" ||
-        s == "http" ||
-        s == "https" ||
-        s == "ws" ||
-        s == "wss" ||
-        s == "ftp" ||
-        s == "data"
+fn is_domain_like(s: &str) -> bool {
+    !s.starts_with('/') && s.contains('/') ||
+        (!s.contains(' ') && !s.starts_with('.') && s.split('.').count() > 1)
+}
+
+fn is_localhost(s: &str) -> bool {
+    s == "localhost"
 }

ports/servoshell/test.rs
@@ -114,6 +114,12 @@ fn test_cmdline_and_location_bar_url() {
         "data:text/html,a",
     );
     test_url(
+        "servo:config",
+        "servo:config",
+        "servo:config",
+        "servo:config",
+    );
+    test_url(
         "README.md",
         "https://readme.md/",
         "file:///fake/cwd/README.md",
@@ -171,6 +177,12 @@ fn test_cmdline_and_location_bar_url() {
         "data:text/html,a",
         "data:text/html,a",
         "data:text/html,a",
+    );
+    test_url(
+        "servo:config",
+        "servo:config",
+        "servo:config",
+        "servo:config",
     );
     test_url(
         "README.md",